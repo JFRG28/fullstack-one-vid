7cde5f6b4a1e2372382158c0ff8adca38a18122b
JFRG <fgrodriguez.28@gmail.com>
2025-11-19 23:32:14 -0600

    using functions and button

--- js_basics/index.js
+++ js_basics/index.js
@@ -1,18 +1,29 @@
 //document.getElementById("count-el").innerText = 5
 
-let count = 0
-console.log(count)
 
-let myAge=34
-let humanDogRatio=7
-let myDogAge=myAge*humanDogRatio
-console.log(myDogAge)
+//console.log(count)
+//
+//let myAge=34
+//let humanDogRatio=7
+//let myDogAge=myAge*humanDogRatio
+//console.log(myDogAge)
+//
+//let bonusPoints=50
+//console.log(bonusPoints)
+//bonusPoints=bonusPoints+50
+//console.log(bonusPoints)
+//bonusPoints=bonusPoints-75
+//console.log(bonusPoints)
+//bonusPoints=bonusPoints+45
+//console.log(bonusPoints)
 
-let bonusPoints=50
-console.log(bonusPoints)
-bonusPoints=bonusPoints+50
-console.log(bonusPoints)
-bonusPoints=bonusPoints-75
-console.log(bonusPoints)
-bonusPoints=bonusPoints+45
-console.log(bonusPoints)
+let count=0
+let countEl=document.getElementById("count-el")
+console.log(countEl)
+
+function increment(){
+    console.log("The button was clicked")
+    count++
+    countEl.innerText=count
+    console.log(count)
+}
