bf7c9125e3746564eb75aff4ca494e3756277992
JFRG <fgrodriguez.28@gmail.com>
2025-11-19 22:54:08 -0600

    declaration, initialization, log and basic operations with variables

---
+++ js_basics/index.js
@@ -0,0 +1,18 @@
+//document.getElementById("count-el").innerText = 5
+
+let count = 0
+console.log(count)
+
+let myAge=34
+let humanDogRatio=7
+let myDogAge=myAge*humanDogRatio
+console.log(myDogAge)
+
+let bonusPoints=50
+console.log(bonusPoints)
+bonusPoints=bonusPoints+50
+console.log(bonusPoints)
+bonusPoints=bonusPoints-75
+console.log(bonusPoints)
+bonusPoints=bonusPoints+45
+console.log(bonusPoints)
