af0e1f69248d353defaa3f2964c66ed2044f7279
JFRG28 <fgrodriguez.28@gmail.com>
2025-11-24 17:49:03 -0600

    added practices: error msg and calculator. Created new folder for the solo practice: basketball scoreboard

--- js_basics/error_msg/index.js
+++ js_basics/error_msg/index.js
@@ -2,4 +2,9 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let errEl=document.getElementById("error")
+let errMsgDesc="Something went wrong, please try again"
 
+function errorMsg(){
+    errEl.textContent=errMsgDesc
+}
