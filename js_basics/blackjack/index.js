0f239e892aec8d7504db431d5c672a468dc8d12d
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-01 16:45:48 -0600

    added: language, charset and sum process to render on index.html

--- js_basics/blackjack/index.js
+++ js_basics/blackjack/index.js
@@ -1,22 +1,24 @@
-let firstCard=123
+let firstCard=1
 let secondCard=11
 let sum=firstCard+secondCard
 let hasBlackJack=false
 let isAlive=true
 let message=""
+let messageEl=document.getElementById("message-el")
+let sumEl=document.getElementById("sum-el")
 
 function startGame(){
+    sumEl.textContent="Sum: "+sum
     if (sum<=20){
     message="Do you want to draw a new card? 🙂"
-} else if (sum===21){
-    message="Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack=true
-} else {
+    } else if (sum===21){
+        message="Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack=true
+    } else {
     message="You're out of the game! 😭"
     isAlive=false
-}
+    }
+    messageEl.textContent=message
 }
 
-console.log(message)
-console.log(hasBlackJack)
-console.log(isAlive)
+
