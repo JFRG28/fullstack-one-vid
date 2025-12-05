f5c416075bbf02b0f2d0f6ceda404feab4e9cd2a
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-04 18:08:53 -0600

    added default value in index.js according to property min of the input field

--- js_basics/password_gen/index.js
+++ js_basics/password_gen/index.js
@@ -1,6 +1,5 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-const defaultLength=15
 let isCustomLength=false
 let pwd1=document.querySelector("#pwd1");
 let pwd2=document.querySelector("#pwd2");
@@ -33,12 +32,12 @@ function renderPwd(){
 }
 
 function genPwd(){
-    let length=defaultLength
+    let length=lengthInput.min
     let password=""
     if (isCustomLength){
         length=lengthInput.valueAsNumber
     } else {
-        length=defaultLength
+        length=lengthInput.min
     }
     for (let i=0;i<length;i++){
         password+=characters[Math.floor(Math.random()*characters.length)]
