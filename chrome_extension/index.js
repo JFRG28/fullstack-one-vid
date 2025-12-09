66f09534b851f932f17b3aeaef17f410e1d1e592
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-09 14:09:32 -0600

    added innerHTML for ul

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -17,11 +17,13 @@ inputBtn.addEventListener("click", function(){
     console.log(myLeads);
     */
     myLeads.push(inputEl.value);
-    //console.log(myLeads);
+    console.log(myLeads);
 })
 
 for (let i=0;i<myLeads.length;i++){
-    console.log(myLeads[i]);
+    //console.log(myLeads[i]);
+    //ulEl.textContent+=myLeads[i]+" ";
+    ulEl.innerHTML+="<li>"+myLeads[i]+"</li>";
 }
 
 
