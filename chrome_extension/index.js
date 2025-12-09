7077075b3f21c6554db457246a90b89887984649
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-09 13:10:32 -0600

    refactored button  to addEventListener

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,6 +1,15 @@
-let msgBtnClick=""
+let msgBtnClick="";
+let inputBtn=document.querySelector("#input-btn");
 
-function saveInput(){
-    msgBtnClick="Button clicked!"
+/*function saveInput(){
+    msgBtnClick="Button clicked! from onclick attribute"
     console.log(msgBtnClick)
 }
+*/
+
+inputBtn.addEventListener("click", function(){
+        msgBtnClick="Button clicked! from addEventListener"
+        console.log(msgBtnClick)
+})
+
+
