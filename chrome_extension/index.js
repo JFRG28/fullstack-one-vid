c7cb39d266688ba446deea2d6bd0af9c33dc5d52
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-09 13:50:40 -0600

    using array to store the text from the input field. Using a for loop to log out the content of the array

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,7 +1,7 @@
 let msgBtnClick="";
 const inputBtn=document.querySelector("#input-btn");
 let myLeads=[];
-const inputEl=document.querySelector("#input-el");
+const inputEl=document.querySelector("#input-el")
 
 /*function saveInput(){
     msgBtnClick="Button clicked! from onclick attribute"
@@ -10,8 +10,16 @@ const inputEl=document.querySelector("#input-el");
 */
 
 inputBtn.addEventListener("click", function(){
-        msgBtnClick="Button clicked! from addEventListener";
-        console.log(msgBtnClick);
+    /*msgBtnClick="Button clicked! from addEventListener";
+    console.log(msgBtnClick);
+    myLeads.push("www.awesomelead.com")
+    console.log(myLeads);
+    */
+    myLeads.push(inputEl.value);
+    //console.log(myLeads);
+    for (let i=0;i<myLeads.length;i++){
+        console.log(myLeads[i]);
+    }
 })
 
 
