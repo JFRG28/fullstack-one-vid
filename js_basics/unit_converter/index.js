b264433a4b1555cd5ae70429e809af23bac1ba8e
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-12 17:24:30 -0600

    created function converter using parameters

--- js_basics/unit_converter/index.js
+++ js_basics/unit_converter/index.js
@@ -1,11 +1,14 @@
 const convertBtn=document.querySelector("#convert-btn")
-const oneFeet=3.28084
+const inputEl=document.querySelector("#input-el")
+const oneFeetInMeters=3.28084
+const oneMeterInFeet=0.3048
 
 convertBtn.addEventListener("click",function(){
-    console.log("button clicked")
+    console.log("mt to feet= "+converter(Number(inputEl.value),oneFeetInMeters))
+    console.log("feet to mt= "+converter(Number(inputEl.value),oneMeterInFeet))
 })
 
-function mtToFeet(){
-
-
+function converter(qt,destUnit){
+    let conversion=Number(qt)*destUnit    
+    return conversion.toFixed(2)
 }
