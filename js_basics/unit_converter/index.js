68489d39e418050957e01fdfd74796b5c1699021
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-15 17:19:11 -0600

    equivalences are corrected

--- js_basics/unit_converter/index.js
+++ js_basics/unit_converter/index.js
@@ -1,17 +1,17 @@
 const convertBtn=document.querySelector("#convert-btn")
 const inputEl=document.querySelector("#input-el")
-const oneFeetToMeters=3.28084
-const oneMeterToFeet=0.3048
-const oneLiterToImpGallon=0.219969
-const oneImpGallonToLiters=4.54609
-const oneKgToPounds=35.274
-const onePoundToKgs=0.02834955374248
+const oneMeterToFeet=3.28084
+const oneFeetToMeters=0.3048
+const oneLiterToUSAGallon=0.264172
+const oneUSAGallonToLiters=3.78541
+const oneKgToPounds=2.20462
+const onePoundToKgs=0.453592
 
 convertBtn.addEventListener("click",function(){
-    console.log("mt to feet= "+converter(Number(inputEl.value),oneFeetToMeters))
-    console.log("feet to mt= "+converter(Number(inputEl.value),oneMeterToFeet))
-    console.log("lt to gallImp= "+converter(Number(inputEl.value),oneLiterToImpGallon))
-    console.log("gallImp to lt = "+converter(Number(inputEl.value),oneImpGallonToLiters))
+    console.log("mt to feet= "+converter(Number(inputEl.value),oneMeterToFeet))
+    console.log("feet to mt= "+converter(Number(inputEl.value),oneFeetToMeters))
+    console.log("lt to USAgall= "+converter(Number(inputEl.value),oneLiterToUSAGallon))
+    console.log("USAgall to lt = "+converter(Number(inputEl.value),oneUSAGallonToLiters))
     console.log("kg to pounds= "+converter(Number(inputEl.value),oneKgToPounds))
     console.log("pound to kgs = "+converter(Number(inputEl.value),onePoundToKgs))
 })
