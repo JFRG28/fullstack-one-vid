e5a04804744c0fff062e0c8b382be4a9448acc11
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-15 17:05:59 -0600

    added missing equivalences

--- js_basics/unit_converter/index.js
+++ js_basics/unit_converter/index.js
@@ -3,11 +3,17 @@ const inputEl=document.querySelector("#input-el")
 const oneFeetToMeters=3.28084
 const oneMeterToFeet=0.3048
 const oneLiterToImpGallon=0.219969
-const oneImpGallonToLiters=0.219969
+const oneImpGallonToLiters=4.54609
+const oneKgToPounds=35.274
+const onePoundToKgs=0.02834955374248
 
 convertBtn.addEventListener("click",function(){
     console.log("mt to feet= "+converter(Number(inputEl.value),oneFeetToMeters))
     console.log("feet to mt= "+converter(Number(inputEl.value),oneMeterToFeet))
+    console.log("lt to gallImp= "+converter(Number(inputEl.value),oneLiterToImpGallon))
+    console.log("gallImp to lt = "+converter(Number(inputEl.value),oneImpGallonToLiters))
+    console.log("kg to pounds= "+converter(Number(inputEl.value),oneKgToPounds))
+    console.log("pound to kgs = "+converter(Number(inputEl.value),onePoundToKgs))
 })
 
 function converter(qt,destUnit){
