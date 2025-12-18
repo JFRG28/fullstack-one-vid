4113200d12052e67256c14e1fa4c01724462c817
JFRG28 <fgrodriguez.28@gmail.com>
2025-12-18 13:30:37 -0600

    corrected aria-live in the button

--- acc_basics/skynet_eats/index.js
+++ acc_basics/skynet_eats/index.js
@@ -3,14 +3,7 @@ function sendMessage() {
     const button = document.getElementById('submitButton');
     
     // Replace the button with a paragraph
-    button.outerHTML = `
-        <div id="announce" aria-live="polite">
-            <p 
-                id="submitMessage" class="submit-message">Message sent! ✅
-            </p>
-        </div>
-        `
-    
+    button.outerHTML = '<p id="submitMessage" class="submit-message">Message sent! ✅</p>';
     // Get the home link element and add focus to it
     
 }
